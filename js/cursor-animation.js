36621946c06eca4f485d31bca81eed54ca543ff3
ttyomma <artemkolisnychenko@gmail.com>
2025-09-04 15:35:43 +0300

    Initial commit: portfolio website

--- js/cursor-animation.js
+++ js/cursor-animation.js
@@ -1,8 +1,11 @@
-window.addEventListener('DOMContentLoaded', (event) => {
+document.addEventListener('DOMContentLoaded', () => {
     const body = document.body;
+    const isTouchDevice = 'ontouchstart' in window || navigator.maxTouchPoints > 0;
 
-    window.addEventListener('mousemove', e => {
-        body.style.setProperty('--x', e.clientX + 'px');
-        body.style.setProperty('--y', (e.clientY + window.scrollY) + 'px');
-    });
+    if (!isTouchDevice) {
+        window.addEventListener('mousemove', e => {
+            body.style.setProperty('--x', e.clientX + 'px');
+            body.style.setProperty('--y', (e.clientY + window.scrollY) + 'px');
+        });
+    }
 });
